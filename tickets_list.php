e562eaf4822c980e7e5a891023600c37e33aad0f
Coordinación Sistemas de Información <86372619+coordsistemasinfo@users.noreply.github.com>
2024-11-07 12:07:53 -0500

    Ajuste de formato

--- tickets_list.php
+++ tickets_list.php
@@ -1,4 +1,53 @@
 <?php
+
+/**
+ * Valida la referencia HTTP de la solicitud.
+ *
+ * Esta función verifica si el valor de la cabecera `HTTP_REFERER` está presente y si el dominio
+ * de la URL referida está en la lista de dominios permitidos. Se utiliza para verificar si
+ * la solicitud proviene de una fuente confiable.
+ *
+ * @return bool Devuelve `true` si el dominio de la referencia está en la lista de dominios permitidos, 
+ *              o `false` si no está presente o no coincide con los dominios válidos.
+ *
+ * @note Los dominios permitidos incluyen:
+ *       - unicomfacauca.edu.co
+ *       - mail.google.com
+ *       - outlook.office.com
+ *       - outlook.live.com
+ *       - localhost
+ *       - 127.0.0.1
+ */
+function validarReferencia()
+{
+
+    if (isset($_SERVER['HTTP_REFERER'])) {  // Verifica si la cabecera HTTP_REFERER está presente
+
+
+
+        // Obtiene la URL del referer
+        $referer = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+        // Define los dominios válidos
+        $dominiosPermitidos = [
+            'unicomfacauca.edu.co',
+            'localhost',
+            '127.0.0.1'
+        ];
+
+        // Verifica si el dominio coincide o si es un subdominio permitido
+        foreach ($dominiosPermitidos as $dominio) {
+            if ($referer === $dominio || substr($referer, -strlen($dominio) - 1) === ".$dominio") {
+                return true;
+            }
+        }
+    }else{
+        return in_array($_SERVER['SERVER_NAME'],['localhost','127.0.0.1','::1']);
+    }
+    return false;
+}
+$_PERMITIR_DESDE_URL_REFERIDA = validarReferencia();
+
 define("INCLUDE_DIR", dirname(__FILE__) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR);
 include './include/ost-config.php';
 
@@ -10,7 +59,22 @@ if ($DB->connect_error) {
     die("Error de conexión: " . $DB->connect_error);
 }
 
-$userEmail = mb_strtolower(trim($_GET['user']??'coordsistemasinfo@unicomfacauca.edu.co'));
+if (isset($_GET['uid'])) {
+    $_WHERE = "t.user_id  = {$_GET['uid']}";
+    $_SQL_USER_EMIAL = "SELECT ue.address 
+                   FROM ost_user u 
+                   JOIN ost_user_email ue  ON ue.id = u.default_email_id 
+                   WHERE u.id = '{$_GET['uid']}'";
+    if ($result = $DB->query($_SQL_USER_EMIAL)) {
+        if ($user = $result->fetch_object()) {
+            $userEmail = $user->address;
+        }
+    }
+} else {
+    $userEmail = mb_strtolower(trim($_GET['user'] ?? 'coordsistemasinfo@unicomfacauca.edu.co'));
+    $_WHERE = "ue.address = '{$userEmail}'";
+}
+
 
 $fromAndwhere = "
 FROM ost_ticket t
@@ -20,7 +84,7 @@ FROM ost_ticket t
     JOIN ost_ticket_status s ON s.id = t.status_id 
     JOIN ost_ticket__cdata td ON td.ticket_id = t.ticket_id 
     JOIN ost_user_email ue ON ue.user_id = t.user_id
-WHERE ue.address = '{$userEmail}'";
+WHERE $_WHERE ";
 
 // Variables para la paginación
 $itemsPerPage = $_GET['per_page'] ?? 15; // Número de tickets por página
@@ -56,6 +120,13 @@ SELECT
     t.source,
     t.status_id,
     s.name status,
+    CASE s.state
+        WHEN 'open' THEN 'primary'
+        WHEN 'closed' THEN 'success'
+        WHEN 'archived' THEN 'secondary'
+        WHEN 'deleted' THEN 'danger'
+        ELSE 'info '
+    END state,
     DATE_FORMAT(t.created, '%e/%m/%Y %I:%i %p') created,
     DATE_FORMAT(t.closed , '%e/%m/%Y %I:%i %p') closed
     {$fromAndwhere}
@@ -75,7 +146,7 @@ $ticketsResult = $DB->query($ticketsQuery);
     <link rel="icon" type="image/png" href="/images/oscar-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="/images/oscar-favicon-16x16.png" sizes="16x16" />
     <!-- Bootstrap CSS -->
-    <link type="text/css" rel="stylesheet"  href="/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="/css/bootstrap.min.css" />
     <style>
         .truncate-text {
             white-space: nowrap;
@@ -85,13 +156,25 @@ $ticketsResult = $DB->query($ticketsQuery);
             text-overflow: ellipsis;
             /* Agrega "..." al final del texto truncado */
             max-width: 250px;
-            /* Ajusta el ancho máximo de la columna */
+            /* Ajusta el ancho máximo de la columna */            
+        }
+        .bold{
+            font-weight: bold;
         }
     </style>
 </head>
 
 <body>
     <div class="container-fluid my-4">
+        <?php if (!$_PERMITIR_DESDE_URL_REFERIDA): ?>
+            <div class="alert alert-danger" role="alert">
+                <h4 class="alert-heading">Acceso restringido</h4>
+                <p>Este enlace para ver los tickets no está disponible desde este medio.
+                    Puede acceder al sistema en <a href="https://siu.unicomfacauca.edu.co">https://siu.unicomfacauca.edu.co</a> y selecciona una de las opciones de Soporte para ver esta información.
+                </p>
+            </div>
+            <?php die("</div></body></html>") ?>
+        <?php endif; ?>
         <h1 class="mb-4">Listado de Tickets</h1>
         <div class="d-flex justify-content-between align-items-center mb-3">
             <span>Mostrando
@@ -100,7 +183,7 @@ $ticketsResult = $DB->query($ticketsQuery);
                         <option <?= ($number == $itemsPerPage ? 'selected' : '') ?>><?= $number ?></option>
                     <?php endforeach; ?>
                 </select>
-                Registros, del <?= $from ?> al <?= $to ?> de <?= $totalTickets ?> tickets. Registrados por el usuario: <strong><?=$userEmail?></strong></span>
+                Registros, del <?= $from ?> al <?= $to ?> de <?= $totalTickets ?> tickets. Registrados por el usuario: <strong><?= $userEmail ?></strong></span>
         </div>
         <!-- Tabla de tickets -->
         <table class="table table-bordered table-striped table-hover small">
@@ -110,7 +193,7 @@ $ticketsResult = $DB->query($ticketsQuery);
                     <th>Número</th>
                     <th>Creado el</th>
                     <th>Departamento</th>
-                    <th>Tema</th>                    
+                    <th>Tema</th>
                     <th>Asunto</th>
                     <th>Fuente</th>
                     <th>Estado</th>
@@ -121,13 +204,13 @@ $ticketsResult = $DB->query($ticketsQuery);
                 <?php while ($ticket = $ticketsResult->fetch_assoc()): ?>
                     <tr title="<?= htmlspecialchars($ticket['subject']) ?>">
                         <td><?= $from++ ?></td>
-                        <td><a href="/timeline.php?tid=<?= $ticket['ticket_id'] ?>"><?= htmlspecialchars($ticket['number']) ?></a></td>
+                        <td class="bold"><a href="/timeline.php?tid=<?= $ticket['ticket_id'] ?>"><?= htmlspecialchars($ticket['number']) ?></a></td>
                         <td><?= htmlspecialchars($ticket['created']) ?></td>
                         <td><?= htmlspecialchars($ticket['departament']) ?></td>
-                        <td><?= htmlspecialchars($ticket['topic']) ?></td>                        
-                        <td class="truncate-text"><a href="/timeline.php?tid=<?= $ticket['ticket_id'] ?>"><?= htmlspecialchars($ticket['subject']) ?></a></td>
+                        <td><?= htmlspecialchars($ticket['topic']) ?></td>
+                        <td class="truncate-text bold"><a href="/timeline.php?tid=<?= $ticket['ticket_id'] ?>"><?= htmlspecialchars($ticket['subject']) ?></a></td>
                         <td><?= htmlspecialchars($ticket['source']) ?></td>
-                        <td><?= htmlspecialchars($ticket['status']) ?></td>
+                        <td><span class="badge  rounded-pill bg-<?= $ticket['state'] ?>"><?= htmlspecialchars($ticket['status']) ?></span></td>
                         <td><?= htmlspecialchars($ticket['closed']) ?></td>
                     </tr>
                 <?php endwhile; ?>
@@ -149,19 +232,19 @@ $ticketsResult = $DB->query($ticketsQuery);
 
                 // Enlace "Anterior"
                 if ($page > 1) {
-                    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page - 1) . '&per_page=' . $itemsPerPage . '&user='.$userEmail.'">Anterior</a></li>';
+                    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page - 1) . '&per_page=' . $itemsPerPage . '&user=' . $userEmail . '">Anterior</a></li>';
                 }
 
                 // Enlaces de número de página
                 for ($i = $startPage; $i <= $endPage; $i++) {
                     echo '<li class="page-item ' . ($i == $page ? 'active' : '') . '">';
-                    echo '<a class="page-link" href="?page=' . $i . '&per_page=' . $itemsPerPage . '&user='.$userEmail.'">' . $i . '</a>';
+                    echo '<a class="page-link" href="?page=' . $i . '&per_page=' . $itemsPerPage . '&user=' . $userEmail . '">' . $i . '</a>';
                     echo '</li>';
                 }
 
                 // Enlace "Siguiente"
                 if ($page < $totalPages) {
-                    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page + 1) . '&per_page=' . $itemsPerPage . '&user='.$userEmail.'">Siguiente</a></li>';
+                    echo '<li class="page-item"><a class="page-link" href="?page=' . ($page + 1) . '&per_page=' . $itemsPerPage . '&user=' . $userEmail . '">Siguiente</a></li>';
                 }
                 ?>
             </ul>
@@ -169,10 +252,11 @@ $ticketsResult = $DB->query($ticketsQuery);
     </div>
 
     <script>
-        document.getElementById('per_page').addEventListener('change', function() {            
+        document.getElementById('per_page').addEventListener('change', function() {
             var perPage = this.value; // Obtener el valor seleccionado            
             var url = new URL(window.location.href); // Obtener la URL actual            
             url.searchParams.set('per_page', perPage); // Establecer o actualizar el parámetro 'per_page' con el valor seleccionado            
+            url.searchParams.set('page', '1');
             window.location.href = url; // Recargar la página con la nueva URL
         });
     </script>
--- timeline.php
+++ timeline.php
@@ -1,4 +1,96 @@
 <?php
+
+/**
+ * Valida la referencia HTTP de la solicitud.
+ *
+ * Esta función verifica si el valor de la cabecera `HTTP_REFERER` está presente y si el dominio
+ * de la URL referida está en la lista de dominios permitidos. Se utiliza para verificar si
+ * la solicitud proviene de una fuente confiable.
+ *
+ * @return bool Devuelve `true` si el dominio de la referencia está en la lista de dominios permitidos, 
+ *              o `false` si no está presente o no coincide con los dominios válidos.
+ *
+ * @note Los dominios permitidos incluyen:
+ *       - unicomfacauca.edu.co
+ *       - mail.google.com
+ *       - outlook.office.com
+ *       - outlook.live.com
+ *       - localhost
+ *       - 127.0.0.1
+ */
+function validarReferencia()
+{
+
+    if (isset($_SERVER['HTTP_REFERER'])) {  // Verifica si la cabecera HTTP_REFERER está presente        
+
+
+
+        // Obtiene la URL del referer
+        $referer = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+        // Define los dominios válidos
+        $dominiosPermitidos = [
+            'unicomfacauca.edu.co',
+            'mail.google.com',
+            'outlook.office.com',
+            'outlook.live.com',
+            'localhost',
+            '127.0.0.1'
+        ];
+
+
+        // Verifica si el dominio coincide o si es un subdominio permitido
+        foreach ($dominiosPermitidos as $dominio) {
+            if ($referer === $dominio || substr($referer, -strlen($dominio) - 1) === ".$dominio") {
+                return true;
+            }
+        }
+    } else {
+        return in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1', '::1']);
+    }
+
+    return false;
+}
+
+/**
+ * Valida si una fecha de cierre no es null y si han pasado más de 6 meses desde esa fecha.
+ *
+ * @param string|null $fechaCierre Fecha de cierre en formato 'YYYY-mm-dd HH:ii:ss'.
+ *                                Puede ser null.
+ * @return bool Retorna `true` si la fecha no es null y han pasado más de 6 meses; 
+ *              de lo contrario, retorna `false`.
+ *
+ * ### Descripción:
+ * La función primero verifica si la fecha de cierre es `null`. Si es así, retorna `false`.
+ * Luego convierte `$fechaCierre` a un objeto `DateTime` y calcula una fecha límite
+ * restando 6 meses a la fecha actual. Si `$fechaCierre` es anterior a esta fecha límite,
+ * retorna `true`; de lo contrario, `false`.
+ */
+function validarFechaCierre($fechaCierre)
+{
+    // Verifica si la fecha no es null
+    if ($fechaCierre === null) {
+        return false;
+    }
+
+    // Convierte la fecha en un objeto DateTime
+    $fechaCierreObj = new DateTime($fechaCierre);
+
+    // Obtiene la fecha actual y resta 6 meses
+    $fechaLimite = new DateTime();
+    $fechaLimite->modify('-6 months');
+
+    // Compara la fecha de cierre con la fecha límite
+    return $fechaCierreObj < $fechaLimite;
+}
+
+
+$_PERMITIR_ACCESO_SIN_RESTICION_TIEMPO = validarReferencia();
+// Uso de la función
+
+
+
+
 $ticketId = $_GET['tid'] ?? 2347;
 
 include './get-file.php';
@@ -28,6 +120,15 @@ if ($result = $DB->query($_SQL_TICKET)) {
     }
 }
 
+// se esta accediendo a esta pagina sin provedir de url de dominio unicomfacauca.edu.co talvez desde un link de correo
+// Por lo cual se validara si el ticket tiene menos de 6 meses de cerrado para permitir ver contendido del ticket
+if (!$_PERMITIR_ACCESO_SIN_RESTICION_TIEMPO) {
+    $_PERMITIR_VER_CONTENIDO = validarFechaCierre($ticket->closed);
+} else {
+    $_PERMITIR_VER_CONTENIDO = TRUE;
+}
+
+
 $_SQL_ENTRYS =
     "SELECT * FROM (
     SELECT
@@ -48,7 +149,8 @@ $_SQL_ENTRYS =
         END AS source ,
         te.title ,
         te.body ,
-        DATE_FORMAT(te.created, '%a, %d %b %Y a las %I:%i %p')  created
+        DATE_FORMAT(te.created, '%a, %d %b %Y a las %I:%i %p')  created,
+        te.created creado
     FROM ost_thread_entry te
     WHERE te.thread_id = {$thread->id}	
     UNION 
@@ -60,15 +162,17 @@ $_SQL_ENTRYS =
         'web' source,
         e.description title,
         te.data body,
-        DATE_FORMAT(te.`timestamp`, '%a, %d %b %Y a las %I:%i %p')  created
+        DATE_FORMAT(te.`timestamp`, '%a, %d %b %Y a las %I:%i %p')  created,
+        te.`timestamp` creado
     FROM ost_thread_event te
     LEFT JOIN ost_staff st ON st.staff_id = te.staff_id 
     JOIN ost_event e ON e.id = te.event_id 
     WHERE te.thread_id = {$thread->id}
    ) timeline 
-   ORDER BY timeline.created ASC";
+   ORDER BY timeline.creado DESC";
 
 if ($entries = $DB->query($_SQL_ENTRYS)) {
+  #  echo $_SQL_ENTRYS;
 }
 
 ?>
@@ -83,12 +187,12 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
     <link rel="icon" type="image/png" href="/images/oscar-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="/images/oscar-favicon-16x16.png" sizes="16x16" />
     <!-- Bootstrap CSS -->
-    <link type="text/css" rel="stylesheet"  href="/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="/css/bootstrap.min.css" />
 
     <title>Ticket # <?= $ticketId ?></title>
     <style>
         .timeline-with-icons {
-            border-left: 1px solid hsl(0, 0%, 90%);
+            border-left: 3px solid hsl(0, 0%, 90%);
             position: relative;
             list-style: none;
         }
@@ -105,15 +209,20 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
 
         .timeline-with-icons .timeline-icon {
             position: absolute;
-            left: -48px;
+            left: -54px;
             background-color: hsl(217, 88.2%, 90%);
             color: hsl(217, 88.8%, 35.1%);
             border-radius: 50%;
-            height: 31px;
-            width: 31px;
+            height: 41px;
+            width: 41px;
             display: flex;
             align-items: center;
             justify-content: center;
+        }
+
+        .span-pre {
+            font-size: 0.875em;
+            margin-bottom: 1rem;
         }
     </style>
     <script type="text/javascript" src="/js/jquery-3.7.0.min.js?ca95150"></script>
@@ -123,9 +232,24 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
 <body>
 
     <div class="container mt-4">
+
+        <?php if (!$_PERMITIR_VER_CONTENIDO) : ?>
+            <div class="alert alert-danger" role="alert">
+                <h4 class="alert-heading">Este enlace ya no está disponible</h4>
+                <p>Este enlace para ver el ticket ya no está disponible desde este medio, ya que ha expirado. Los enlaces de tickets expiran 6 meses después de que se cierran. Para ver los detalles de este ticket, accede al sistema en <a href="https://siu.unicomfacauca.edu.co">https://siu.unicomfacauca.edu.co</a> y selecciona una de las opciones de Soporte.</p>
+                <hr>
+                <p class="mb-0">El ticket fue cerrado el: <?php echo $ticket->closed ? date('d/m/Y H:i:s', strtotime($ticket->closed)) : 'N/A'; ?>.</p>
+            </div>
+            <?php die("</div></body></html>") ?>
+        <?php endif; ?>
+
         <?php if (!empty($_SERVER['HTTP_REFERER'])): ?>
             <div class="text-end mb-2">
                 <a class="btn btn-primary" href="<?= $_SERVER['HTTP_REFERER'] ?>"><i class="fa-solid fa-rotate-left"></i> Regresar a la página anterior</a>
+            </div>
+        <?php else: ?>
+            <div class="text-end mb-2">
+                <a class="btn btn-primary" href="/tickets_list.php?uid=<?= $ticket->user_id ?>"><i class="fa-solid fa-list-check"></i> Lista de Tickets</a>
             </div>
         <?php endif; ?>
         <div class="card">
@@ -145,14 +269,14 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
                     <div class="col-md-6">
                         <p><strong>Staff:</strong> <?php echo $ticket->staff ?? 'No asignado'; ?></p>
                         <p><strong>Equipo:</strong> <?php echo $ticket->team ?? 'N/A'; ?></p>
-                        <p><strong>Fecha de Creación:</strong> <?php echo date('d/m/Y H:i:s', strtotime($ticket->created)); ?></p>
-                        <p><strong>Última Actualización:</strong> <?php echo date('d/m/Y H:i:s', strtotime($ticket->lastupdate)); ?></p>
-                        <p><strong>Fecha de Cierre:</strong> <?php echo $ticket->closed ? date('d/m/Y H:i:s', strtotime($ticket->closed)) : 'N/A'; ?></p>
+                        <p><strong>Fecha de Creación:</strong> <?php echo date('d/m/Y h:i A', strtotime($ticket->created)); ?></p>
+                        <p><strong>Última Actualización:</strong> <?php echo date('d/m/Y h:i A', strtotime($ticket->lastupdate)); ?></p>
+                        <p><strong>Fecha de Cierre:</strong> <?php echo $ticket->closed ? date('d/m/Y h:i A', strtotime($ticket->closed)) : 'N/A'; ?></p>
                     </div>
                 </div>
             </div>
             <div class="card-footer text-muted">
-                <i class="fas fa-info-circle"></i> Información del ticket # <?php echo $ticket->number; ?>, Generada el <?= date('Y-m-d H:i:s') ?>
+                <i class="fas fa-info-circle"></i> Información del ticket # <?php echo $ticket->number; ?>, Creado el <?php echo date('d/m/Y h:i A', strtotime($ticket->created)); ?>
             </div>
         </div>
         <!-- Section: Timeline -->
@@ -161,16 +285,16 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
                 <?php while ($entry = $entries->fetch_object()): ?>
 
                     <?php if (!in_array($entry->type, ['event', 'N'])): ?>
-                        <li class="timeline-item mb-4 mt-1">
+                        <li class="timeline-item mb-4 mt-3">
                             <hr />
                             <span class="timeline-icon">
                                 <?php if ($entry->staff_id): ?>
-                                    <i class="fa-solid fa-life-ring text-blue-800 fa-sm fa-fw"></i>
+                                    <i class="fa-solid fa-life-ring text-blue-800 fa-fw"></i>
                                 <?php else: ?>
-                                    <i class="fa-solid fa-user text-blue-800 fa-sm fa-fw"></i>
+                                    <i class="fa-solid fa-user text-blue-800 fa-fw "></i>
                                 <?php endif; ?>
                             </span>
-                            <h5 class="fw-bold">
+                            <h5 class="fw-bold text-primary">
                                 <i class="<?= $entry->source ?>"></i>
                                 <?= $entry->poster ?> : <?= $entry->title ?>
                             </h5>
@@ -205,11 +329,13 @@ if ($entries = $DB->query($_SQL_ENTRYS)) {
                         </li>
 
                     <?php elseif ($entry->type == 'event'): ?>
-                        <li class="timeline-item mb-3 mt-3">
+                        <li class="timeline-item mb-4 mt-3">
+                        <hr />
                             <span class="timeline-icon">
-                                <i class="fa-solid fa-pen text-indigo-800 fa-sm fa-fw"></i>
+                                <i class="fa-solid fa-pencil text-indigo-800 fa-fw"></i>
                             </span>
-                            <p class="mb-2"><?= $entry->poster ?>, Tipo Edición: <?= $entry->title ?>, <?= $entry->body ?>, el: <?= $entry->created ?></p>
+                            <p class="mb-2"><i class="fa-solid fa-user"></i> <strong><?= $entry->poster ?></strong>, <i class="fa-solid fa-pencil"></i> <strong><?= $entry->title?$entry->title:'' ?></strong><?= $entry->title?', ':'' ?>
+                            <?= $entry->body?'<i class="fa-solid fa-keyboard"></i>':''?> <span class="span-pre"><?= $entry->body?json_encode(json_decode($entry->body), JSON_UNESCAPED_UNICODE).',':'' ?></span> <i class="fa-solid fa-calendar-day"></i> El: <?= $entry->created ?></p>
                         </li>
                     <?php endif; ?>
 
